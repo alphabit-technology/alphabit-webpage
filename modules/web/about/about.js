88c65f90ba95a2ba560636309e410ffc300fa2b8
Alfredo <alfredrz2012@gmail.com>
2023-07-20 22:07:52 -0400

    initial-commit

---
+++ modules/web/about/about.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import {BaseDocument} from 'loopar-env';
+
+export default class About extends BaseDocument {
+    constructor(props){
+        super(props);
+    }
+}
---
+++ modules/web/about/client/about-view.js
@@ -0,0 +1,13 @@
+
+'use strict';
+import WebContext from '/gui/document/web-context.js'
+
+export default class AboutView extends WebContext {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return super.render()
+    }
+}
