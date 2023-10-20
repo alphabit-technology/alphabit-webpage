256f68c1c025352d644422d1007c41937319cd75
Alfredo <alfredrz2012@gmail.com>
2023-10-19 22:28:02 -0400

    Update to Version 0.1.6

---
+++ modules/web/donate/client/donate-view.js
@@ -0,0 +1,10 @@
+
+'use strict';
+
+import WebContext from '/gui/document/web-context.js';
+
+export default class DonateView extends WebContext {
+    constructor(props){
+        super(props);
+    }
+}
